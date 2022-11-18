fa431af39652cde3a46cb093dac16421ab2ad09e
Marlon j. Frausto <marlonjfrausto@gmail.com>
2022-11-18 22:12:12 +0100

    testing layout dawgy

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tailwindui.com',
+        port: '',
+        pathname: '/img/logos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      
+    ],
+  },
 }
 
 module.exports = nextConfig
