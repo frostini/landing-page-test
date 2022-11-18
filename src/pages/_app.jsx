9647964d6f587cace4adb3516c2431b9bab5acee
Marlon j. Frausto <marlonjfrausto@gmail.com>
2022-11-18 15:22:02 +0100

    added the list methods and the create methods for the Posts, as well as the amplify create account/login widgets. need to update graphql schema to include the content field

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import '@aws-amplify/ui-react/styles.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
