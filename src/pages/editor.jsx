31f280901bb18f414f591f32c01bf65d774730c1
Marlon j. Frausto <marlonjfrausto@gmail.com>
2022-11-22 16:50:46 +0100

    removed the stale package and related route to complete build and remove the wyswy plugin not is not going to be used

--- src/pages/editor.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Wysimark, useEditor } from '@wysimark/react'
-
-const MyComponent = () => {
-  const editor = useEditor({ initialMarkdown: "# Hello World" })
-  return <Wysimark editor={editor} />
-}
-
-export default MyComponent;
