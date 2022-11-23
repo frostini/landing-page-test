2e7fa4cb1851de8cbedf205487c4761b513888b3
Marlon j. Frausto <marlonjfrausto@gmail.com>
2022-11-23 16:25:13 +0100

    going to test elements in production.

--- src/pages/drafts/[id].jsx
+++ src/pages/drafts/[id].jsx
@@ -1,55 +1,31 @@
-import GovAppShell from '@/layout/GovAppShell'
-
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
 import { Fragment } from 'react'
-import { Menu, Popover, Transition } from '@headlessui/react'
+import { Menu, Transition } from '@headlessui/react'
 import {
-  ArrowLongLeftIcon,
   CheckIcon,
   HandThumbUpIcon,
-  HomeIcon,
-  MagnifyingGlassIcon,
   PaperClipIcon,
   QuestionMarkCircleIcon,
   UserIcon,
 } from '@heroicons/react/20/solid'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import GovAppShell from '@/layout/GovAppShell'
+import Breadcrumbs from '@/components/Breadcrumbs'
 
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+const items = [
+  { name: 'Save and schedule', href: '#' },
+  { name: 'Save and publish', href: '#' },
+  { name: 'Export PDF', href: '#' },
+]
 const user = {
   name: 'Whitney Francis',
   email: 'whitney@example.com',
   imageUrl:
     'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
 }
-const navigation = [
-  { name: 'Dashboard', href: '#' },
-  { name: 'Jobs', href: '#' },
-  { name: 'Applicants', href: '#' },
-  { name: 'Company', href: '#' },
-]
-const breadcrumbs = [
-  { name: 'Jobs', href: '#', current: false },
-  { name: 'Front End Developer', href: '#', current: false },
-  { name: 'Applicants', href: '#', current: true },
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
 const attachments = [
   { name: 'resume_front_end_developer.pdf', href: '#' },
   { name: 'coverletter_front_end_developer.pdf', href: '#' },
@@ -125,78 +101,16 @@ const comments = [
   },
 ]
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
 export default function Example() {
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
       <div className="min-h-full">
-        <header className="bg-white shadow">
-
-
-          <div className="mx-auto max-w-7xl px-4 sm:px-6">
-            <div className="border-t border-gray-200 py-3">
-              <nav className="flex" aria-label="Breadcrumb">
-                <div className="flex sm:hidden">
-                  <a
-                    href="#"
-                    className="group inline-flex space-x-3 text-sm font-medium text-gray-500 hover:text-gray-700"
-                  >
-                    <ArrowLongLeftIcon
-                      className="h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-600"
-                      aria-hidden="true"
-                    />
-                    <span>Back to Applicants</span>
-                  </a>
-                </div>
-                <div className="hidden sm:block">
-                  <ol role="list" className="flex items-center space-x-4">
-                    <li>
-                      <div>
-                        <a href="#" className="text-gray-400 hover:text-gray-500">
-                          <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
-                          <span className="sr-only">Home</span>
-                        </a>
-                      </div>
-                    </li>
-                    {breadcrumbs.map((item) => (
-                      <li key={item.name}>
-                        <div className="flex items-center">
-                          <svg
-                            className="h-5 w-5 flex-shrink-0 text-gray-300"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            aria-hidden="true"
-                          >
-                            <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
-                          </svg>
-                          <a
-                            href={item.href}
-                            className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-                            aria-current={item.current ? 'page' : undefined}
-                          >
-                            {item.name}
-                          </a>
-                        </div>
-                      </li>
-                    ))}
-                  </ol>
-                </div>
-              </nav>
-            </div>
+        <header className="shadow">
+          <div className='p-5'>
+            <Breadcrumbs/>
           </div>
         </header>
+
 
         <main className="py-10">
           {/* Page header */}
@@ -224,6 +138,56 @@ export default function Example() {
               </div>
             </div>
             <div className="justify-stretch mt-6 flex flex-col-reverse space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-y-0 sm:space-x-3 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
+
+
+
+    <div className="inline-flex rounded-md">
+      <button
+        type="button"
+        className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+      >
+        Save changes
+      </button>
+      <Menu as="div" className="relative -ml-px block">
+        <Menu.Button className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-3 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500">
+          <span className="sr-only">Open options</span>
+          <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 -mr-1 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              {items.map((item) => (
+                <Menu.Item key={item.name}>
+                  {({ active }) => (
+                    <a
+                      href={item.href}
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      {item.name}
+                    </a>
+                  )}
+                </Menu.Item>
+              ))}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+
+
+
+
               <button
                 type="button"
                 className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
--- src/pages/drafts/create.jsx
+++ src/pages/drafts/create.jsx
@@ -1,4 +1,5 @@
 import GovAppShell from '@/layout/GovAppShell'
+import Breadcrumbs from '@/components/Breadcrumbs'
 /*
   This example requires some changes to your config:
   
@@ -16,41 +17,6 @@ import GovAppShell from '@/layout/GovAppShell'
 import { ChevronRightIcon, HomeIcon } from '@heroicons/react/20/solid'
 import { CheckIcon } from '@heroicons/react/24/solid'
 
-const pages = [
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Project Nero', href: '#', current: true },
-]
-
-function Breadcrumbies() {
-  return (
-    <nav className="flex" aria-label="Breadcrumb">
-      <ol role="list" className="flex items-center space-x-4">
-        <li>
-          <div>
-            <a href="#" className="text-gray-400 hover:text-gray-500">
-              <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
-              <span className="sr-only">Home</span>
-            </a>
-          </div>
-        </li>
-        {pages.map((page) => (
-          <li key={page.name}>
-            <div className="flex items-center">
-              <ChevronRightIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-              <a
-                href={page.href}
-                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
-                aria-current={page.current ? 'page' : undefined}
-              >
-                {page.name}
-              </a>
-            </div>
-          </li>
-        ))}
-      </ol>
-    </nav>
-  )
-}
 
 
 
@@ -186,7 +152,7 @@ export default function Create() {
       <Stepper/>
       </div>
       <div className='p-5'>
-      <Breadcrumbies/>
+      <Breadcrumbs/>
       </div>
       <div className='p-10'>
         <div className="md:grid md:grid-cols-3 md:gap-6">
